57dc9e0b797992b6f81092bb8315bf55e33b50cd
z130 <546361420@qq.com>
2017-04-18 11:43:53 +0800

    Add files via upload

--- src/demo/StringDemo.java
+++ src/demo/StringDemo.java
@@ -3,7 +3,7 @@ package demo;
 public class StringDemo {
 	public StringDemo() {
 	}
-	public String smallString(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -12,7 +12,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString1(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString1(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -21,7 +21,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString2(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString2(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -30,7 +30,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString3(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString3(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -39,7 +39,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString4(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString4(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -48,7 +48,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString5(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString5(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -57,7 +57,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString6(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString6(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -66,7 +66,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString7(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString7(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -75,7 +75,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString8(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString8(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
@@ -84,7 +84,7 @@ public class StringDemo {
 		String s = str.toLowerCase();
 		return s;
 	}
-	public String smallString9(String str)// ×Ö·û´®±äÐ¡Ð´
+	public String smallString9(String str)
 	{
 		String temp = "error";
 		if (str.equals("") || str == null) {
--- src/demo/testStringDemo1.java
+++ src/demo/testStringDemo1.java
@@ -9,43 +9,43 @@ public class testStringDemo1 extends TestCase {
 		str = new StringDemo();
 	}
 		public void testSmallString() {
-		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		assertEquals("1",str.smallString("A"),"a");
 		
 	}
 		public void testSmallString1() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 		public void testSmallString2() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 		public void testSmallString3() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 		public void testSmallString4() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 		public void testSmallString5() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 		public void testSmallString6() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 		public void testSmallString7() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 		public void testSmallString8() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 		public void testSmallString9() {
-			assertEquals("一个字母变小写",str.smallString("A"),"a");
+			assertEquals("1",str.smallString("A"),"a");
 			
 		}
 }
